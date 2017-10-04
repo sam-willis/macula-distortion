e4f302ed6ec8f5dbb7725ff42a5e41707313c6dc
Sam Willis <samuel.j.willis@outlook.com>
2017-10-04 18:00:24 +0100

    refacting (slightly) of remapper.py, bringing apply_macula_distortion() out of main()

--- src/remapper.py
+++ src/remapper.py
@@ -9,24 +9,23 @@ NO_FLAG = 0
 INV_FLAG = 2**4
 
 def main():
-    img_path = 'images/circle_image.jpg'
+    import matplotlib.pyplot as plt
+
+    img_path = 'images/test.png'
     img = ndimage.imread(img_path)
+    plt.axis('off')
+    plt.imshow(apply_macula_distortion(img))
+    plt.show()
+
+def apply_macula_distortion(img):
     w, h = img.shape[:2]
-    
     polar_img = cv2.linearPolar(img, (h//2, w//2), sqrt(w**2+h**2)/2, NO_FLAG)
-    
     polar_img = np.flip(polar_img, 1)
     reverse_map = create_reverse_map(polar_img)
     warped = warp(polar_img, reverse_map)
     warped = np.flip(warped, 1)
-
-    img_macl = cv2.linearPolar(warped, (h//2, w//2), sqrt(w**2+h**2)/2, INV_FLAG)
-
-    import matplotlib.pyplot as plt
-    plt.axis('off')
-    plt.imshow(img_macl)
-    plt.show()
-
+    return cv2.linearPolar(warped, (h//2, w//2), sqrt(w**2+h**2)/2, INV_FLAG)
+    
 
 def create_reverse_map(img):
     _, height, _ = img.shape
